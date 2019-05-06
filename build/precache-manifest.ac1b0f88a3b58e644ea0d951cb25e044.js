9120b6e90de54cdd1fc5bad6324caa0db0647855
jlomoglio <joe.lomoglio@gmail.com>
2019-05-06 15:08:40 -0400

    added build folder to repo

---
+++ build/precache-manifest.ac1b0f88a3b58e644ea0d951cb25e044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f7a8e002e391c1cf5b9bc035e4ee16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97b24d1376eb47edcab1",
+    "url": "/static/css/main.f47fa5d7.chunk.css"
+  },
+  {
+    "revision": "bf675104cc6995c546e5",
+    "url": "/static/js/2.d8912289.chunk.js"
+  },
+  {
+    "revision": "97b24d1376eb47edcab1",
+    "url": "/static/js/main.6c12c46b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
